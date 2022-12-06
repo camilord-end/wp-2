adf65744fdf0555e5d994639c0bc68896fb35ed3
Julian Orozco <juliancaor10@gmail.com>
2022-12-06 14:36:26 -0500

    Add emailValidator functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,9 @@ import IconButton from '@mui/material/IconButton'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import SearchIcon from '@mui/icons-material/Search'
 import TextField from '@mui/material/TextField'
+import * as EmailValidator from 'email-validator'
+
+import { auth } from '../firebase'
 
 export const Sidebar = () => {
   const headerStyles = {
@@ -19,11 +22,22 @@ export const Sidebar = () => {
     padding: '15px',
     borderBottom: '1px solid whitesmoke'
   }
+  const avatarStyles = { cursor: 'pointer', '&:hover': { opacity: 0.8 } }
+
+  const createChat = () => {
+    const input = prompt(
+      'Please enter an email addres for the user to chat with: '
+    )
+    if (!input) return null
+    if (EmailValidator.validate(input)) {
+      //todo- need to add the chat db collection
+    }
+  }
 
   return (
     <Box>
       <Box sx={headerStyles}>
-        <Avatar sx={{ cursor: 'pointer', '&:hover': { opacity: 0.8 } }} />
+        <Avatar onClick={() => auth.signOut()} sx={avatarStyles} />
         <Box>
           <IconButton>
             <ChatIcon />
@@ -42,7 +56,12 @@ export const Sidebar = () => {
           sx={{ flex: 1 }}
         />
       </Box>
-      <Button variant='text' color='success' sx={{ width: '100%' }}>
+      <Button
+        variant='text'
+        color='success'
+        onClick={createChat}
+        sx={{ width: '100%' }}
+      >
         Start a new Chat
       </Button>
       {/* List of active chats */}
