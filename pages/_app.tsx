2606dc969c257ebe2563ad942526739d7118a42e
Julian Orozco <juliancaor10@gmail.com>
2022-12-06 14:35:44 -0500

    Add auth logic implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,42 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+import { auth, db } from '../firebase'
+import {
+  collection,
+  doc,
+  setDoc,
+  updateDoc,
+  serverTimestamp
+} from 'firebase/firestore'
+import { Loading } from '../components/Loading'
+import { Login } from './login'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useEffect } from 'react'
+
 import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const [user, loading] = useAuthState(auth)
+  useEffect(() => {
+    if (user) {
+      //todo
+      setNewUser(doc(collection(db, 'users'), user.uid), {
+        email: user.email,
+        lastSeen: serverTimestamp(),
+        photoUrl: user.photoURL,
+        name: user.displayName
+      })
+    }
+  }, [user])
+
+  const setNewUser = async (newUserRef: any, data: any) => {
+    await setDoc(newUserRef, data)
+  }
+
+  if (loading) return <Loading />
+  if (!user) return <Login />
   return <Component {...pageProps} />
 }
 
