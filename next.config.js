c4efd31e22151be0fe54607c41bf5ea496edaed1
Julian Orozco <juliancaor10@gmail.com>
2022-12-12 12:32:17 -0500

    Whitelist google hostname

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'upload.wikimedia.org'
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
       }
     ]
   }
