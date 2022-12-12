472bc5b5a56453658d830a8a50859390cf99f5f3
Julian Orozco <juliancaor10@gmail.com>
2022-12-12 12:33:09 -0500

    ChatScreen boilerplate

---
+++ components/ChatScreen.tsx
@@ -0,0 +1,9 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+export const ChatScreen = () => {
+  return (
+    <Box>
+      <Typography>This is a chat</Typography>
+    </Box>
+  )
+}
