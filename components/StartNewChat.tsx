e170fd8573e3fa9439edee07fbd0525503485695
Julian Orozco <juliancaor10@gmail.com>
2022-12-12 12:34:14 -0500

    Add Start New Chat Component

---
+++ components/StartNewChat.tsx
@@ -0,0 +1,26 @@
+import Button from '@mui/material/Button'
+import { collection, query, where } from 'firebase/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { auth, db } from '../firebase'
+import { createChat } from '../utils/createChat'
+
+export const StartNewChat = () => {
+  const [user] = useAuthState(auth)
+  const collectionRef = collection(db, 'chats')
+  const userQuery = query(
+    collectionRef,
+    where('users', 'array-contains', user?.email)
+  )
+  const [chatsSnapShot] = useCollection(userQuery)
+  return (
+    <Button
+      variant='text'
+      color='success'
+      onClick={() => createChat(chatsSnapShot, user, collectionRef)}
+      sx={{ width: '100%' }}
+    >
+      Start a new Chat
+    </Button>
+  )
+}
