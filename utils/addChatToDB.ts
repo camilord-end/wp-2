5ab749a1510b344505554f3182f15705f16a2059
Julian Orozco <juliancaor10@gmail.com>
2022-12-15 17:24:53 -0500

    Add addChatToDB service

--- utils/addChatToDB.ts
+++ utils/addChatToDB.ts
@@ -3,6 +3,6 @@ import { addDoc, CollectionReference, DocumentData } from 'firebase/firestore'
 export const addChatToDB = async (
   newUserRef: CollectionReference<DocumentData>,
   data: unknown
-) => {
+): Promise<void> => {
   await addDoc(newUserRef, data)
 }
