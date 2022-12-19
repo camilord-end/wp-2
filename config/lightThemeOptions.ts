00493b9b7893e26de55841be154e01dcd0c33b9c
Julian Orozco <juliancaor10@gmail.com>
2022-12-19 17:29:39 -0500

    Add light theme options

---
+++ config/lightThemeOptions.ts
@@ -0,0 +1,18 @@
+import { ThemeOptions } from '@mui/material/styles'
+
+const lightThemeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#25063f'
+    },
+    secondary: {
+      main: '#c30c4e'
+    },
+    background: {
+      default: '#f4e4ff'
+    }
+  }
+}
+
+export default lightThemeOptions
