7ede2dfce1b0291067ecda4dee4468e8fa34ef4c
Julian Orozco <juliancaor10@gmail.com>
2022-12-19 17:30:23 -0500

    Add button to toggle themes

--- components/SidebarHeaderButtons.tsx
+++ components/SidebarHeaderButtons.tsx
@@ -2,15 +2,18 @@ import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
 import ChatIcon from '@mui/icons-material/Chat'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { useTheme } from '@mui/material/styles'
+import { ColorModeContext } from '../hooks/ColorModeContext'
+import { useContext } from 'react'
 
 export const SidebarHeaderButtons = () => {
+  const theme = useTheme()
+  const colorMode = useContext(ColorModeContext)
+
   return (
     <Box>
-      <IconButton>
-        <ChatIcon />
-      </IconButton>
-      <IconButton>
-        <MoreVertIcon />
+      <IconButton onClick={colorMode.toggleColorMode}>
+        {theme.palette.mode === 'dark' ? <MoreVertIcon /> : <ChatIcon />}
       </IconButton>
     </Box>
   )
