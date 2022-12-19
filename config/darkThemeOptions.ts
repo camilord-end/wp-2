4c55ed7b0a2a9d105755900facdf6280a67376ba
Julian Orozco <juliancaor10@gmail.com>
2022-12-19 17:29:52 -0500

    Add dark theme options

---
+++ config/darkThemeOptions.ts
@@ -0,0 +1,19 @@
+import { ThemeOptions } from '@mui/material/styles'
+
+const darkThemeOptions: ThemeOptions = {
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#b0f5ff'
+    },
+    secondary: {
+      main: '#f48fb1'
+    },
+    background: {
+      default: '#1d0431',
+      paper: '#3e085c'
+    }
+  }
+}
+
+export default darkThemeOptions
